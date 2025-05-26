69b32707c443adca728c6fb33512d86f3946c351
Nidhin R <nidhinrajesh1998@gmail.com>
2025-05-27 01:04:39 +0530

    Folder Structure Creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generate AI Project",
+    version="0.0.0",
+    author="Nidhin R",
+    auther_email="nidhinrajesh1998@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
